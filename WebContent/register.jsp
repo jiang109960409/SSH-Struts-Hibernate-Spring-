474af4b07fb0c2e32e9d3c047cd044aa97d7cc4d
outSider <1164342035@qq.com>
2016-12-27 15:43:30 +0800

    Add files via upload
    The first version.

---
+++ WebContent/register.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>SSH-Test</title>
+</head>
+<body>
+	<form action="register" method="post">
+		<input type="text" name="user.userName"><br /> 
+		<input type="text" name="user.password"><br /> 
+		<input type="submit" value="注册">
+	</form>
+</body>
+</html>
---
+++ src/com/test/entity/User.java
@@ -0,0 +1,47 @@
+package com.test.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = "t_user")
+public class User {
+
+	private int id;
+	private String userName;
+	private String password;
+
+	@Id
+	@GeneratedValue(generator = "increment")
+	@GenericGenerator(name = "increment", strategy = "increment")
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	@Column(length=20)
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
